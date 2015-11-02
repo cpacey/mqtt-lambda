35e96890b9a7ab33c5c8d06509388dd2d8520782
Jeff Ashton <jeff.ashton@cantire.com>
2015-11-02 14:35:45 -0500

    Adding support for custom args

---
+++ bin/argsParser.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var _ = require( 'lodash' );
+var argsLib = require( 'args' );
+
+var argvOptions = argsLib.Options.parse( [
+    {
+        name: 'brokerUrl',
+        help: 'The mqtt broker url (or via $MQTT_BROKER_URL)'
+    },
+    {
+        name: 'subscribersRoot',
+        help: 'The root directory for the subscribers (or via $SUBSCRIBERS_ROOT)'
+    },
+    {
+        name: 'clientId',
+        help: 'The mqtt client id (or via $MQTT_CLIENT_ID)'
+    },
+    {
+        name: 'clean',
+        type: 'bool',
+        defaultValue: true,
+        help: 'Whether or not the client should create a clean session (or via $MQTT_CLIENT_CLEAN)'
+    }
+] );
+
+var argvMap = {
+    brokerUrl: 'MQTT_BROKER_URL',
+    subscribersRoot: 'SUBSCRIBERS_ROOT',
+    clientId: 'MQTT_CLIENT_ID',
+    clean: 'MQTT_CLIENT_CLEAN'
+};
+
+function parseArgs( argv, env ) {
+
+    var args = argsLib.parser( argv ).parse( argvOptions );
+    _.forIn( args, function( value, key ) {
+
+        var envName = argvMap[ key ];
+        if( !envName ) {
+            throw new Error( 'unknown argument ' + key );
+        }
+
+        env[ envName ] = value;
+    } );
+}
+
+module.exports = parseArgs;
---
+++ test/argsParserTests.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var assert = require( 'assert' );
+var argsParser = require( '../bin/argsParser.js' );
+
+describe( 'argsParser', function() {
+
+    describe( 'no arts', function() {
+        it( 'should be parsed', function() {
+
+            var argv = [ 'node', 'test.js' ];
+            var env =  {};
+
+            argsParser( argv, env );
+
+            assert.deepEqual( env, {
+                MQTT_CLIENT_CLEAN: true
+            } );
+        } );
+    } );
+
+    describe( 'brokerUrl', function() {
+        it( 'should be parsed', function() {
+
+            var argv = [ 'node', 'test.js', '--brokerUrl', 'mqtt://test.org' ];
+            var env =  {};
+
+            argsParser( argv, env );
+
+            assert.deepEqual( env, {
+                MQTT_CLIENT_CLEAN: true,
+                MQTT_BROKER_URL: 'mqtt://test.org'
+            } );
+        } );
+    } );
+
+    describe( 'subscribersRoot', function() {
+        it( 'should be parsed', function() {
+
+            var argv = [ 'node', 'test.js', '--subscribersRoot', './test/' ];
+            var env =  {};
+
+            argsParser( argv, env );
+
+            assert.deepEqual( env, {
+                MQTT_CLIENT_CLEAN: true,
+                SUBSCRIBERS_ROOT: './test/'
+            } );
+        } );
+    } );
+
+    describe( 'clientId', function() {
+        it( 'should be parsed', function() {
+
+            var argv = [ 'node', 'test.js', '--clientId', 'abc' ];
+            var env =  {};
+
+            argsParser( argv, env );
+
+            assert.deepEqual( env, {
+                MQTT_CLIENT_CLEAN: true,
+                MQTT_CLIENT_ID: 'abc'
+            } );
+        } );
+    } );
+
+    describe( 'clean=true', function() {
+        it( 'should be parsed', function() {
+
+            var argv = [ 'node', 'test.js', '--clean', 'true' ];
+            var env =  {};
+
+            argsParser( argv, env );
+
+            assert.deepEqual( env, {
+                MQTT_CLIENT_CLEAN: true
+            } );
+        } );
+    } );
+
+    describe( 'clean=false', function() {
+        it( 'should be parsed', function() {
+
+            var argv = [ 'node', 'test.js', '--clean', 'false' ];
+            var env =  {};
+
+            argsParser( argv, env );
+
+            assert.deepEqual( env, {
+                MQTT_CLIENT_CLEAN: false
+            } );
+        } );
+    } );
+} );
